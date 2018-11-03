6a828913a698ecd27d13bca6e5e6fa6ac2d5e574
taramouse <nickykeyse@gmail.com>
2018-11-03 15:22:49 +0000

    Added new genres.

--- src/util/genres.js
+++ src/util/genres.js
@@ -1,9 +1,11 @@
 module.exports = Object.freeze({
+  ACTION:  'Action',
   ANIMATION: 'Animation',
   COMEDY: 'Comedy',
   CRIME: 'Crime',
   DOCUMENTARY: 'Documentary',
   DRAMA: 'Drama',
   FANTASY: 'Fantasy',
-  HORROR: 'Horror'
+  HORROR: 'Horror',
+  SCIFI: 'Sci-Fi'
 });
