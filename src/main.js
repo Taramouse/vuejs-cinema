896cb03c7758c881be64121d2ccf8c3c8aa81714
taramouse <nickykeyse@gmail.com>
2018-11-03 13:45:21 +0000

    Check filter event processed.

--- src/main.js
+++ src/main.js
@@ -9,9 +9,20 @@ new Vue({
     MovieList,
     MovieFilter
   },
+  data: {
+    genre: [],
+    time: []
+  },
   methods: {
     checkFilter(category, title, checked) {
-      console.log(category, title, checked)
+      if(checked) {
+        this[category].push(title)
+      } else {
+        let index = this[category].indexOf(title)
+        if(index > -1) {
+          this[category].splice(index, 1)
+        }
+      }
     }
   }
 })
