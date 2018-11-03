350f630a2f802b7891366e9deb6ea43afed74e0f
taramouse <nickykeyse@gmail.com>
2018-11-03 12:10:06 +0000

    Imported component check filter.

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -1,15 +1,20 @@
 <template>
 <div id="movie-filter">
-  {{msg}}
+  <h2>Filter Results</h2>
+  <check-filter></check-filter>
 </div>
 </template>
 
 <script>
+import CheckFilter from './CheckFilter.vue'
 export default {
- name: 'movie-list',
+ name: 'movie-filter',
+ components: {
+   CheckFilter
+ },
  data() {
    return {
-     msg: 'I am movie-filter'
+     // msg: 'I am movie-filter'
    }
  }
 }
