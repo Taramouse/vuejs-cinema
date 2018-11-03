4838f5f1266e6e5a8186a13d17f9e93fe22c4ce0
taramouse <nickykeyse@gmail.com>
2018-11-03 12:49:40 +0000

    Added filter genres and checkboxes.

--- src/components/CheckFilter.vue
+++ src/components/CheckFilter.vue
@@ -1,9 +1,13 @@
 <template>
-<div>Filter</div>
+<div class="check-filter">
+  <span class="checkbox"></span>
+  <span class="check-filter-title">{{title}}</span>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'check-filter'
+  name: 'check-filter',
+  props: ['title']
 }
 </script>
--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -1,12 +1,16 @@
 <template>
 <div id="movie-filter">
   <h2>Filter Results</h2>
-  <check-filter></check-filter>
+  <div class="filter-group">
+    <check-filter v-for="genre in genres" v-bind:key="genre" v-bind:title="genre"></check-filter>
+  </div>
 </div>
 </template>
 
 <script>
 import CheckFilter from './CheckFilter.vue'
+import Genres from '../util/genres'
+
 export default {
  name: 'movie-filter',
  components: {
@@ -14,7 +18,7 @@ export default {
  },
  data() {
    return {
-     // msg: 'I am movie-filter'
+     genres: Genres
    }
  }
 }
