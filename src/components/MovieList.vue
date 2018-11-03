92b7db05184654a9dcfc8c2ba8367ff0226bb2a5
taramouse <nickykeyse@gmail.com>
2018-11-03 14:44:03 +0000

    Added filter logic.

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,25 +1,38 @@
 <template>
 <div id="movie-list">
-  <div class="movie" v-for="movie in movies" v-bind:key="movie.title">
+  <div class="movie" v-for="movie in filteredMovies" v-bind:key="movie.title">
     {{movie.title}}
   </div>
 </div>
 </template>
 
 <script>
-import genres from '../util/genres'
+import Genres from '../util/genres'
 
 export default {
  name: 'movie-list',
  props: ['genre', 'time'],
  data() {
    return {
-     genres,
      movies: [
-       { title: 'The Avengers', genre: genres.FANTASY },
-       { title: 'Pulp Fiction', genre: genres.CRIME },
-       { title: 'Austin Powers', genre: genres.COMEDY }
+       { title: 'The Avengers', genre: Genres.FANTASY },
+       { title: 'Pulp Fiction', genre: Genres.CRIME },
+       { title: 'Austin Powers', genre: Genres.COMEDY }
      ]
+   }
+ },
+ methods: {
+   moviePassesGenreFilter(movie) {
+     if(!this.genre.length) {
+       return true
+     } else {
+       return this.genre.find(genre => movie.genre === genre)
+     }
+   }
+ },
+ computed: {
+   filteredMovies() {
+     return this.movies.filter(this.moviePassesGenreFilter)
    }
  }
 }
