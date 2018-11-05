3fb242a41d33a6adbe5f00e50dc1baa84554f3e0
taramouse <nickykeyse@gmail.com>
2018-11-05 12:38:06 +0000

    Set timezone to UTC with moment.

--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@ import './style.scss'
 import MovieList from './components/MovieList.vue'
 import MovieFilter from './components/MovieFilter.vue'
 import VueResource from 'vue-resource'
+import moment from 'moment-timezone'
 
 Vue.use(VueResource)
+
+// Set default timezone with moment
+moment.tz.setDefault('UTC')
+// Moment is not a vue module so cannot do Vue.use()
+// To make moment globally accessible to all components.
+Object.defineProperty(Vue.prototype, '$moment', {get() {
+    return this.$root.moment
+  }
+})
 
 new Vue({
   el: '#app',
@@ -15,7 +25,8 @@ new Vue({
   data: {
     genre: [],
     time: [],
-    movies: []
+    movies: [],
+    moment
   },
   methods: {
     checkFilter(category, title, checked) {
