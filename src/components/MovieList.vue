1afeae4c5006324e7e071f72f3b0931ff6a3d5d4
taramouse <nickykeyse@gmail.com>
2018-11-08 14:25:36 +0000

    Added logic for time filter.

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -19,6 +19,7 @@
 
 <script>
 import genres from '../util/genres'
+import times from '../util/times'
 import MovieItem from './MovieItem.vue'
 
 export default {
@@ -38,11 +39,29 @@ export default {
        })
        return matched
      }
+   },
+   sessionPassesTimeFilter(session) {
+     if(!this.day.isSame(this.$moment(session.time), 'day')) {
+       // If seesion time is not current day
+       return false
+     } else if(this.time.length === 0 || this.time.length === 2) {
+       // If no filters or both filters are checked
+       return true
+     } else if(this.time[0] === times.AFTER_6PM ) {
+       // After 6pm filter selected
+       return this.$moment(session.time).hour() >= 18
+     } else {
+       // Before 6pm filter selected
+       return this.$moment(session.time).hour() < 18
+     }
+
    }
  },
  computed: {
    filteredMovies() {
-     return this.movies.filter(this.moviePassesGenreFilter)
+     return this.movies
+      .filter(this.moviePassesGenreFilter)
+      .filter(movie => movie.sessions.find(this.sessionPassesTimeFilter))
    }
  },
  components: {
