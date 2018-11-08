7fd2a7adc81b9127bcd8705dc1c51e02bc0cae98
taramouse <nickykeyse@gmail.com>
2018-11-08 19:05:13 +0000

    Added movie details

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,6 +1,20 @@
 <template>
  <div id="detail" v-if="movie">
-   <movie-item v-bind:movie="movie.movie"></movie-item>
+   <movie-item v-bind:movie="movie">
+     <div class="movie-details">
+       <p class="movie-genre">{{movie.Genre}}</p>
+       <p class="movie-plot">{{movie.Plot}}</p>
+       <table>
+         <tr><td>Release Date:</td><td>{{movie.Released}}</td></tr>
+         <tr><td>Running Time:</td><td>{{movie.Runtime}}</td></tr>
+         <tr><td>Director:</td><td>{{movie.Director}}</td></tr>
+         <tr><td>Cast:</td><td>{{movie.Actors}}</td></tr>
+       </table>
+     </div>
+   </movie-item>
+   <div class="home">
+     <router-link v-bind:to="{name: 'home'}">Back to results</router-link>
+   </div>
  </div>
 </template>
 
@@ -13,7 +27,7 @@ export default {
     movie() {
       let movie = this.movies.find(movie => movie.id === this.$route.params.id)
       // Check movie is defined
-      return movie ? movie : null
+      return movie ? movie.movie : null
     }
   },
   components: {
