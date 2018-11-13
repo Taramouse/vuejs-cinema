5a14690e05260579a3705da7fafaadf39bb90a08
taramouse <nickykeyse@gmail.com>
2018-11-13 14:20:20 +0000

    Added dynamic seats availble.

--- src/main.js
+++ src/main.js
@@ -62,7 +62,7 @@ let mouseOutHandler = function(event) {
 Vue.directive('tooltip', {
   bind(el, bindings) {
     let span = document.createElement('SPAN')
-    let text = document.createTextNode('Seats available: 200')
+    let text = document.createTextNode(`Seats available: ${bindings.value.seats}`)
     span.appendChild(text)
     addClass(span, 'tooltip')
     el.appendChild(span)
