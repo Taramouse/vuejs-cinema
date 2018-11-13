dd4cdc42fc2d947486e0dd71eff944fcca2f7d28
taramouse <nickykeyse@gmail.com>
2018-11-13 14:27:29 +0000

    Extracted tooltip directive to vue plugin.

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import './style.scss'
 import moment from 'moment-timezone'
 import {checkFilter, setDay} from './util/bus'
 import routes from './util/routes'
+import tooltip from './util/tooltip'
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
+Vue.use(tooltip)
 
 const router = new VueRouter({ routes })
 
@@ -45,38 +47,5 @@ new Vue({
       })
       this.$bus.$on('check-filter', checkFilter.bind(this))
       this.$bus.$on('select-day', setDay.bind(this))
-  }
-})
-
-import {addClass, removeClass} from './util/helpers'
-
-let mouseOverHandler = function(event) {
-  let span = event.target.parentNode.getElementsByTagName('SPAN')[0]
-  addClass(span, 'tooltip-show')
-}
-let mouseOutHandler = function(event) {
-  let span = event.target.parentNode.getElementsByTagName('SPAN')[0]
-  removeClass(span, 'tooltip-show')
-}
-
-Vue.directive('tooltip', {
-  bind(el, bindings) {
-    let span = document.createElement('SPAN')
-    let text = document.createTextNode(`Seats available: ${bindings.value.seats}`)
-    span.appendChild(text)
-    addClass(span, 'tooltip')
-    el.appendChild(span)
-    let div = el.getElementsByTagName('DIV')[0]
-    div.addEventListener('mouseover', mouseOverHandler)
-    div.addEventListener('mouseout', mouseOutHandler)
-    div.addEventListener('touchstart', mouseOverHandler)
-    div.addEventListener('touchend', mouseOutHandler)
-  },
-  unbind(el, bindings) {
-    let div = el.getElementsByTagName('DIV')[0]
-    div.removeEventListener('mouseover', mouseOverHandler)
-    div.removeEventListener('mouseout', mouseOutHandler)
-    div.removeEventListener('touchstart', mouseOverHandler)
-    div.removeEventListener('touchend', mouseOutHandler)
   }
 })
